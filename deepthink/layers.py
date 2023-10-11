92dc8726a9d527c751ee6c79bfcacf718b3eea4a
Caff1982 <stephencafferty@hotmail.com>
2023-10-11 08:59:02 +0100

    Added GlobalAveragePooling2D layer

--- deepthink/layers.py
+++ deepthink/layers.py
@@ -590,14 +590,14 @@ class GlobalAveragePooling1D(BaseLayer):
         self.axis = axis
 
     def __str__(self):
-        return 'GlobalAveragePooling'
+        return 'GlobalAveragePooling1D'
 
     def initialize(self):
         """
         Initialize the global average pooling layer.
         """
         if self.prev_layer is None and self.input_shape is None:
-            raise ValueError('GlobalAveragePooling cannot be the first layer')
+            raise ValueError('GlobalAveragePooling1D cannot be the first layer')
 
         if self.input_shape is None:
             self.input_shape = self.prev_layer.output.shape
@@ -640,6 +640,76 @@ class GlobalAveragePooling1D(BaseLayer):
         self.dinputs = np.ones(grads.shape) * grads / seq_len
         # Broadcast the gradients to the input shape
         self.dinputs = self.dinputs[..., np.newaxis].repeat(seq_len, axis=-1)
+        return self.dinputs
+
+
+class GlobalAveragePooling2D(BaseLayer):
+    """
+    Global Average Pooling 2D layer.
+
+    Parameters
+    ----------
+    axes : tuple, default=(-2, -1)
+        The axes along which the pooling is applied.
+    """
+    def __init__(self, axes=(-2, -1), **kwargs):
+        super().__init__(**kwargs)
+        self.axes = axes
+
+    def __str__(self):
+        return 'GlobalAveragePooling2D'
+
+    def initialize(self):
+        """
+        Initialize the global average pooling 2D layer.
+        """
+        if self.prev_layer is None and self.input_shape is None:
+            raise ValueError('GlobalAveragePooling2D cannot be the first layer')
+
+        if self.input_shape is None:
+            self.input_shape = self.prev_layer.output.shape
+
+        # The output shape will be (batch_size, num_channels)
+        self.output = np.zeros((self.input_shape[0], self.input_shape[1]))
+
+    def forward(self, X):
+        """
+        Perform the forward pass on inputs X.
+
+        Parameters
+        ----------
+        X : array-like, shape (batch_size, features, height, width)
+            Input tensor.
+
+        Returns
+        -------
+        output : array-like, shape (batch_size, features)
+            Average-pooled output tensor.
+        """
+        self.input = X
+        self.output = np.mean(X, axis=self.axes)
+        return self.output
+
+    def backward(self, grads):
+        """
+        Perform backpropagation by computing the gradients.
+
+        Parameters
+        ----------
+        grads : array-like, shape (batch_size, features)
+            Gradients from the subsequent layer.
+
+        Returns
+        -------
+        dinputs : array-like, shape (batch_size, features, height, width)
+            Gradients for the inputs.
+        """
+        height, width = self.input_shape[-2], self.input_shape[-1]
+        # Reshape the gradients to shape (batch_size, num_channels, 1, 1)
+        # and divide by the number of elements in the feature map
+        self.dinputs = grads[:, :, np.newaxis, np.newaxis] / (height * width)
+        # Broadcast the gradients to the input shape
+        self.dinputs = self.dinputs.repeat(height, axis=-2).repeat(width, axis=-1)
         return self.dinputs
 
 
